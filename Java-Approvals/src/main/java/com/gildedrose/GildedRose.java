433b8d99e97cfdf2219e6833d1db0e30e9e81cb5
Sander Verschuren <sander.verschuren@gmail.com>
2023-07-25 17:14:34 +0200

    Move sellIn decreasing to top of method

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -10,17 +10,22 @@ class GildedRose {
     public void updateQuality() {
         for (Item item : items) {
 
+            if (IsSulfuras(item)) {
+            } else {
+                item.sellIn = item.sellIn - 1;
+            }
+
             if(isBackstagePass(item)){
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
 
-                    if (item.sellIn <= 10) {
+                    if (item.sellIn < 10) {
                         if (item.quality < 50) {
                             item.quality = item.quality + 1;
                         }
                     }
 
-                    if (item.sellIn <= 5) {
+                    if (item.sellIn < 5) {
                         if (item.quality < 50) {
                             item.quality = item.quality + 1;
                         }
@@ -37,11 +42,6 @@ class GildedRose {
                         item.quality = item.quality - 1;
                     }
                 }
-            }
-
-            if (IsSulfuras(item)) {
-            } else {
-                item.sellIn = item.sellIn - 1;
             }
 
             if (item.sellIn < 0) {
