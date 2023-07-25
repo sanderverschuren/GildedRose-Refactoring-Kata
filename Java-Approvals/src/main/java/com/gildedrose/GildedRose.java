e2b73384ae8ade6642bd29ca28879a8dcbc265b4
Sander Verschuren <sander.verschuren@gmail.com>
2023-07-25 17:32:45 +0200

    reformat code

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -14,7 +14,7 @@ class GildedRose {
                 item.sellIn = item.sellIn - 1;
             }
 
-            if(isBackstagePass(item)){
+            if (isBackstagePass(item)) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
 
@@ -33,12 +33,11 @@ class GildedRose {
                 if (item.sellIn < 0) {
                     item.quality = 0;
                 }
-            }
-            else if (isAgedBrie(item)) {
+            } else if (isAgedBrie(item)) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
                 }
-                if(item.sellIn < 0){
+                if (item.sellIn < 0) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
                     }
