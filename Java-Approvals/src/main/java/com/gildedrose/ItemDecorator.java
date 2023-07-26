c38d20a4ebb4e27b2eef95c37a4ca1d782a09baa
Sander Verschuren <sander.verschuren@gmail.com>
2023-07-27 00:35:28 +0200

    Too much public constants

--- Java-Approvals/src/main/java/com/gildedrose/ItemDecorator.java
+++ Java-Approvals/src/main/java/com/gildedrose/ItemDecorator.java
@@ -1,12 +1,12 @@
 package com.gildedrose;
 
 public class ItemDecorator {
-    public static final int MAX_QUALITY = 50;
-    public static final int MIN_QUALITY = 0;
-    public static final String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
-    public static final String AGED_BRIE = "Aged Brie";
-    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
-    public static final String CONJURED = "Conjured Mana Cake";
+    private static final int MAX_QUALITY = 50;
+    private static final int MIN_QUALITY = 0;
+    private static final String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private static final String CONJURED = "Conjured Mana Cake";
     private final Item item;
     private final String name;
     private final int quality;
