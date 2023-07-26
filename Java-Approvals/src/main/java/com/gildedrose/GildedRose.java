0108c296cc1c3a8d6596230e8297400b1c5ec5ef
Sander Verschuren <sander.verschuren@gmail.com>
2023-07-26 15:35:23 +0200

    Introduced ItemDecorator

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -3,12 +3,6 @@ package com.gildedrose;
 import java.util.Arrays;
 
 class GildedRose {
-    public static final int MAX_QUALITY = 50;
-    public static final int MIN_QUALITY = 0;
-    public static final String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
-    public static final String AGED_BRIE = "Aged Brie";
-    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
-    public static final String CONJURED = "Conjured Mana Cake";
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -17,59 +11,9 @@ class GildedRose {
 
     public void endDay() {
         items = Arrays.stream(items)
-            .map(GildedRose::updateSellIn)
-            .map(GildedRose::updateQuality)
+            .map(ItemDecorator::new)
+            .map(ItemDecorator::progressToNextDay)
+            .map(ItemDecorator::getItem)
             .toArray(Item[]::new);
-    }
-
-    private static Item updateSellIn(Item item) {
-        int decrement = switch (item.name) {
-            case SULFURAS -> 0;
-            default -> 1;
-        };
-        return new Item(item.name, item.sellIn - decrement, item.quality);
-    }
-
-    private static Item updateQuality(Item item) {
-        int qualityIncrement = switch (item.name) {
-            case SULFURAS -> 0;
-            case BACKSTAGE_PASS -> switch (item) {
-                case Item i when isExpired(i) -> -item.quality;
-                case Item i when isExpiredInDays(i, 5) -> 3;
-                case Item i when isExpiredInDays(i, 10) -> 2;
-                default -> 1;
-            };
-            case AGED_BRIE -> switch (item) {
-                case Item i when isExpired(i) -> 2;
-                default -> 1;
-            };
-            case CONJURED -> switch (item) {
-                case Item i when isExpired(i) -> -4;
-                default -> -2;
-            };
-            default -> switch (item) {
-                case Item i when isExpired(i) -> -2;
-                default -> -1;
-            };
-        };
-        return new Item(item.name, item.sellIn, calculateQuality(item, qualityIncrement));
-    }
-
-    private static boolean isExpiredInDays(Item item, int days) {
-        return item.sellIn < days;
-    }
-
-    private static boolean isExpired(Item item) {
-        return isExpiredInDays(item, 0);
-    }
-
-    private static int calculateQuality(Item item, int increment) {
-        if (increment == 0)
-            return item.quality;
-        return limitQualityRange(item.quality + increment);
-    }
-
-    private static int limitQualityRange(int value) {
-        return Math.min(Math.max(value, MIN_QUALITY), MAX_QUALITY);
     }
 }
---
+++ Java-Approvals/src/main/java/com/gildedrose/ItemDecorator.java
@@ -0,0 +1,80 @@
+package com.gildedrose;
+
+public class ItemDecorator {
+    public static final int MAX_QUALITY = 50;
+    public static final int MIN_QUALITY = 0;
+    public static final String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    public static final String CONJURED = "Conjured Mana Cake";
+    private final Item item;
+    private final String name;
+    private final int quality;
+    private final int sellIn;
+
+    public ItemDecorator(Item item) {
+        this.item = item;
+        this.name = item.name;
+        this.sellIn = item.sellIn;
+        this.quality = item.quality;
+    }
+
+    public Item getItem(){
+        return item;
+    }
+
+    public ItemDecorator progressToNextDay(){
+        return updateSellIn().updateQuality();
+    }
+
+    private ItemDecorator updateSellIn() {
+        int decrement = switch (name) {
+            case SULFURAS -> 0;
+            default -> 1;
+        };
+        return new ItemDecorator(new Item(name, sellIn - decrement, quality));
+    }
+
+    private ItemDecorator updateQuality() {
+        int qualityIncrement = switch (name) {
+            case SULFURAS -> 0;
+            case BACKSTAGE_PASS -> {
+                if (isExpired()) yield -quality;
+                else if (isExpiredInDays(5)) yield 3;
+                else if (isExpiredInDays(10)) yield 2;
+                yield 1;
+            }
+            case AGED_BRIE -> {
+                if (isExpired()) yield 2;
+                yield 1;
+            }
+            case CONJURED -> {
+                if (isExpired()) yield -4;
+                yield -2;
+            }
+            default -> {
+                if (isExpired()) yield -2;
+                yield -1;
+            }
+        };
+        return new ItemDecorator(new Item(name, sellIn, calculateQuality(qualityIncrement)));
+    }
+
+    private boolean isExpiredInDays(int days) {
+        return sellIn < days;
+    }
+
+    private boolean isExpired() {
+        return isExpiredInDays(0);
+    }
+
+    private int calculateQuality(int increment) {
+        if (increment == 0)
+            return quality;
+        return limitQualityRange(quality + increment);
+    }
+
+    private static int limitQualityRange(int value) {
+        return Math.min(Math.max(value, MIN_QUALITY), MAX_QUALITY);
+    }
+}
