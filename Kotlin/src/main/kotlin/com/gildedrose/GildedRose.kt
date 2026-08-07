a8b1fd8894fd82c09ed7424734db3bb14b41bfd5
Sander Verschuren <sander@tripled.io>
2026-08-07 10:28:25 +0200

    Reorder methods

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,8 +4,6 @@ const val AGED_BRIE = "Aged Brie"
 const val BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
 const val SULFURAS = "Sulfuras, Hand of Ragnaros"
 
-private val Item.isExpired get() = sellIn < 0
-
 class GildedRose(val items: List<Item>) {
 
     fun updateQuality() {
@@ -15,33 +13,35 @@ class GildedRose(val items: List<Item>) {
         }
     }
 
-}
+    private fun calculateSellIn(item: Item): Int = when (item.name) {
+        SULFURAS -> item.sellIn
+        else -> item.sellIn - 1
+    }
 
-private fun calculateSellIn(item: Item): Int = when (item.name) {
-    SULFURAS -> item.sellIn
-    else -> item.sellIn - 1
-}
+    private fun calculateQuality(item: Item): Int = when (item.name) {
+        SULFURAS -> 80
+        AGED_BRIE -> calculateAgedBrieQuality(item)
+        BACKSTAGE_PASSES -> calculateBackstagePassesQuality(item)
+        else -> calculateNormalItemQuality(item)
+    }
 
-private fun calculateQuality(item: Item): Int = when (item.name) {
-    SULFURAS -> 80
-    AGED_BRIE -> calculateAgedBrieQuality(item)
-    BACKSTAGE_PASSES -> calculateBackstagePassesQuality(item)
-    else -> calculateNormalItemQuality(item)
-}
+    private fun calculateAgedBrieQuality(item: Item): Int {
+        val increase = if (item.isExpired) 2 else 1
+        return (item.quality + increase).coerceIn(0, 50)
+    }
 
-private fun calculateAgedBrieQuality(item: Item): Int {
-    val increase = if (item.isExpired) 2 else 1
-    return (item.quality + increase).coerceIn(0, 50)
-}
+    private fun calculateBackstagePassesQuality(item: Item): Int = when {
+        item.isExpired -> 0
+        item.sellIn < 5 -> item.quality + 3
+        item.sellIn < 10 -> item.quality + 2
+        else -> item.quality + 1
+    }.coerceIn(0, 50)
 
-private fun calculateBackstagePassesQuality(item: Item): Int = when {
-    item.isExpired -> 0
-    item.sellIn < 5 -> item.quality + 3
-    item.sellIn < 10 -> item.quality + 2
-    else -> item.quality + 1
-}.coerceIn(0, 50)
+    private fun calculateNormalItemQuality(item: Item): Int {
+        val degradation = if (item.isExpired) 2 else 1
+        return (item.quality - degradation).coerceIn(0, 50)
+    }
 
-private fun calculateNormalItemQuality(item: Item): Int {
-    val degradation = if (item.isExpired) 2 else 1
-    return (item.quality - degradation).coerceIn(0, 50)
+    private val Item.isExpired get() = sellIn < 0
+
 }
