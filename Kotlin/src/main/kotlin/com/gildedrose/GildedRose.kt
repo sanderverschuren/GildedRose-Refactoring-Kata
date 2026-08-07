bd89a3c0792f2097ad1497c07d6ac626211d8204
Sander Verschuren <sander@tripled.io>
2026-08-07 10:23:05 +0200

    Update test readibility

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,8 +1,8 @@
 package com.gildedrose
 
-private const val AGED_BRIE = "Aged Brie"
-private const val BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
-private const val SULFURAS = "Sulfuras, Hand of Ragnaros"
+const val AGED_BRIE = "Aged Brie"
+const val BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+const val SULFURAS = "Sulfuras, Hand of Ragnaros"
 
 private val Item.isExpired get() = sellIn < 0
 
--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -10,18 +10,17 @@ internal class GildedRoseTest {
 
     @Test
     fun `Aged Brie with negative quality should be corrected to 0`() {
-        val item = Item("Aged Brie", 3, -5)
-        val app = GildedRose(listOf(item))
+        val agedBrie = Item(AGED_BRIE, 3, -5)
+        val app = GildedRose(listOf(agedBrie))
 
         app.updateQuality()
 
-        val expected = Item("Aged Brie", 2, 0)
-        assertEquals(expected.toString(), item.toString())
+        assertItemsEqual(agedBrie, Item(AGED_BRIE, 2, 0))
     }
 
     @Test
     fun `Backstage passes with negative quality should be corrected to 0`() {
-        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 15, -5))
+        val items = listOf(Item(BACKSTAGE_PASSES, 15, -5))
         val app = GildedRose(items)
 
         app.updateQuality()
@@ -31,7 +30,7 @@ internal class GildedRoseTest {
 
     @Test
     fun `Normal item with quality above 50 should be corrected to 50`() {
-        val items = listOf(Item("+5 Dexterity Vest", 10, 55))
+        val items = listOf(Item("normal item", 10, 55))
         val app = GildedRose(items)
 
         app.updateQuality()
@@ -41,7 +40,7 @@ internal class GildedRoseTest {
 
     @Test
     fun `Sulfuras quality is always 80`() {
-        val items = listOf(Item("Sulfuras, Hand of Ragnaros", 0, 40))
+        val items = listOf(Item(SULFURAS, 0, 40))
         val app = GildedRose(items)
 
         app.updateQuality()
@@ -69,6 +68,8 @@ internal class GildedRoseTest {
         }
         return buffer.toString()
     }
+
+    private fun assertItemsEqual(actual: Item, expected:Item) = assertEquals(expected.toString(), actual.toString())
 
 }
 
